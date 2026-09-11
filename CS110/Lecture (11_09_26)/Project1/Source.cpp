94f973e701a76752e8f8b636a26591e96727f919
Gui Bruno <guiki53u19@gmail.com>
2026-09-11 17:17:09 -0600

    A

--- CS110/Lecture (11_09_26)/Project1/Source.cpp
+++ CS110/Lecture (11_09_26)/Project1/Source.cpp
@@ -9,8 +9,13 @@ string variable_string = "ABCD123";
 bool variable_boolean = true;
 
 int main() {
-	cout << variable_double << endl << variable_string;
-	cout << endl << variable_boolean;
-	cout << endl << variable_double + variable_integer;
+	//cout << variable_double << endl << variable_string;
+	//cout << endl << variable_boolean;
+	//cout << endl << variable_double + variable_integer;
+	int a;
+	cout << "input value for A: ";
+	cin >> a;
+	cout << a + 2;
+	cout << endl << 4 / 30;
 	return 0;
 }
