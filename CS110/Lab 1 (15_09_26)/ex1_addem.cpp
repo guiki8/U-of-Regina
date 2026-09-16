adb63ab03d888c50c71c0133aad321e375b31233
Gui Bruno <guiki53u19@gmail.com>
2026-09-15 18:16:42 -0600

    Lab 1 exercises

---
+++ CS110/Lab 1 (15_09_26)/ex1_addem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "This program adds two integers!" << endl;
+    cout << "Please enter the first number: ";
+    cin >> a;
+    cout << "Please enter the second number: ";
+    cin >> b;
+    cout << "The sum of the two numbers is: " << a + b << endl;
+    return 0;
+}
